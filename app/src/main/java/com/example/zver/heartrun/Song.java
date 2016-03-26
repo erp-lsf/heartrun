aeca409ff6bbb38643f91e4f9d137eaf955f9dd6
erp.lsf <erp.lsf@gmail.com>
2016-03-26 17:11:07 +0200

    AM

---
+++ app/src/main/java/com/example/zver/heartrun/Song.java
@@ -0,0 +1,21 @@
+package com.example.zver.heartrun;
+
+/**
+ * Created by gen76 on 3/26/16.
+ */
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
