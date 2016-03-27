cbf0ae5e6671582e933193ec89b8339446228689
erp.lsf <erp.lsf@gmail.com>
2016-03-27 08:55:10 +0300

    AM

--- app/src/main/java/com/example/zver/heartrun/MainActivity.java
+++ app/src/main/java/com/example/zver/heartrun/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.zver.heartrun;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,11 +23,25 @@ import android.media.MediaPlayer;
 import android.util.Log;
 import android.media.MediaMetadataRetriever;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.net.URL;
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
     protected int heartRate = 80;
     protected MediaPlayer mediaPlayer;
+    protected MediaMetadataRetriever metaRetriever;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,32 +49,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView hr;
-        hr = (TextView)findViewById(R.id.textView);
+        hr = (TextView) findViewById(R.id.textView);
 
         hr.setText("" + heartRate);
 
+//        getPermissions();
+        getSongs();
+
         mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void toggleMusic(View v) {
-        mediaPlayer.start();
-        MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        } else {
+            mediaPlayer.start();
+            metaRetriever = new MediaMetadataRetriever();
 
-//        URI
-//        Log.w("Heartrun", fileURL.getPath());
+            final AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.song);
+            metaRetriever.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
 
-//        metaRetriever.setDataSource(fileURL.getPath());
-//        String artist =  metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
-//        String title = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
-//
-//        TextView st;
-//        st = (TextView)findViewById(R.id.song_title);
-//        st.setText(title);
+            String title = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+
+            Log.w("Heartrun", title);
+
+            TextView st;
+            st = (TextView) findViewById(R.id.song_title);
+            st.setText(title);
+        }
+    }
+
+    protected void playSong(int song_id) {
+
+    }
+
+    // do later
+
+    protected void getSongs() {
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = Uri.parse("content://storage/emulated/0/heartrun/normal");
+
+        Log.w("Heartrun-getSongs", uri.toString());
+
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        if (cursor == null) {
+            // query failed, handle error.
+        } else if (!cursor.moveToFirst()) {
+            // no media on the device
+        } else {
+            int titleColumn = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int idColumn = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+            do {
+                long thisId = cursor.getLong(idColumn);
+                String thisTitle = cursor.getString(titleColumn);
+                Log.w("Heartrate-getSongs", thisTitle);
+                // ...process entry...
+            } while (cursor.moveToNext());
+        }
     }
 
     public void rateUp(View v) {
         TextView hr;
-        hr = (TextView)findViewById(R.id.textView);
+        hr = (TextView) findViewById(R.id.textView);
 
         heartRate++;
         hr.setText("" + heartRate);
@@ -62,10 +121,49 @@ public class MainActivity extends AppCompatActivity {
 
     public void rateDown(View v) {
         TextView hr;
-        hr = (TextView)findViewById(R.id.textView);
+        hr = (TextView) findViewById(R.id.textView);
 
         heartRate--;
         hr.setText("" + heartRate);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zver.heartrun/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.zver.heartrun/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
